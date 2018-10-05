c8d320ea6c875958260a5842cb529233cb60b791
Michael Cumings <michael.cumings@gmail.com>
2018-10-05 14:50:13 -0500

    now working

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
 Bamazon
 
-Video demo (autopsy):
-https://drive.google.com/file/d/1X2HiJUQkW8UJsIjynQGULyCXEwwSLKHz/view
+Video demo:
+https://drive.google.com/file/d/1xjK7rsnbktu2898Tvq_tAVIwrVZqTtOp/view
 
-This is intended to be a Command Line app that mimics store functions.
+This is a Command Line app that mimics store functions.
 
-The bamazoncustomer.js file is intended to print a list of all available
-items at the store (drawing ID numbers, Product Names, and Prices from
-the products table of the bamazon mysql database).  It is then supposed
-to ask for the ID of the product the customer wishes to buy, then the
-quantity of that item, then check the requested quantity against the
+The node server coded in bamazoncustomer.js prints a list of all available
+items at the gaming store (drawing ID numbers, Product Names, and Prices 
+from the products table of the bamazon mysql database).  It then asks 
+for the ID of the product the customer wishes to buy, then the
+quantity of that item, and then check the requested quantity against the
 quantity in the database, and if there is enough quantity to cover the
-order, to calculate and output a total price for the customer and update
+order, it calculates and outputs a total price for the customer and updates
 the products table with the remaining quantity of the purchased item.
 
-It's not doing that now, however.  Right now, it draws the table in the 
-console, but I don't know how to get it to take input and do queries in 
-the necessary sequence to get the functionality I want.  Not only that,
-but I can't get to the debugger in vscode to progress to the point that
-the app gets to in the git bash view of the node console, which makes it
-hard to troubleshoot.  This is explained in the video demo linked above.
+The npm modules I'm using for Bamazon are :
 
-The npm modules I'm using for Bamazon are mysql (to interact with the database),
-inquirer (to handle user inputs), and cli-table (to draw the table to the 
-console).
+mysql (to interact with the database),
+inquirer (to handle user inputs), and 
+cli-table (to draw the table to the console).
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,36 +1,34 @@
+//  node --inspect-brk bamazoncustomer.js
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table")
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "",
   database: "bamazon"
 });
 
-var table = new Table({
-    head: ['ID#', 'Product Name', 'Price']
-// , colWidths: [100, 200, 300]
-});
+
 
 function start() {
   drawtable();
-  // choice();
+
 }
 
 function drawtable() {
-    // instantiate
 
-    table.head = ['ID#', 'Product Name', 'Price'];
+
+
+    var table = new Table({
+      head: ['ID#', 'Product Name', 'Price']
+
+  });
 
     var query = "SELECT item_id, product_name, price FROM products";
     connection.query(query, function(err, res) {
@@ -39,18 +37,17 @@ function drawtable() {
               [res[i].item_id, res[i].product_name, res[i].price]
             );
         }
-        connection.pause();
+
             console.log(table.toString());
             choice();
-        // connection.resume();
+
         return;
-        // choice();
+
       });
 }
 
 function choice() {
-    // console.log(table.toString());
-    connection.resume();
+
     inquirer
     .prompt([
       {
@@ -80,28 +77,29 @@ function choice() {
       var query = "SELECT item_id, stock_quantity, price FROM products WHERE item_id = ?";
       connection.query(query, [answer.itemno], function(err, res) {
         if (err) throw err;
-            // console.log("That's not going to work, try again.");
-            // // choice();
-            // return;
-        var quantity = answer.quantity;
-        var price = answer.price;
+        var quantity = parseInt(answer.quantity);
+        var price = parseFloat(res[0].price);
         var itemno = answer.itemno;
-        var stock_quantity = res.stock_quantity;
-        if (quantity > stock_quantity) {
-            var updated = (res.stock_quantity - quantity);
-            var query = "UPDATE products SET stock_quantity = ?, stock_quantity WHERE item_id = ?";
+        var stock_quantity = res[0].stock_quantity;
+        if (quantity <= stock_quantity) {
+            var updated = (stock_quantity - quantity);
+            var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
             connection.query(query, [updated, itemno], function (err,res) {
                 var total = (quantity * price);
-                console.log("You bought " + quantity + " of item number " + itemno + " for a total cost of " + total + ".");
+                console.log("You bought " + quantity + " of item number " + itemno + " for a total cost of $" + total.toFixed(2) + ".");
             }); 
+            start();
+            return;
         } else {
                 console.log("Not enough quantity in stock to cover that amount!");
+                choice();
                 return;
 
             }
         });
 
       });
+    
     }
 
 
